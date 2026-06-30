2ae912317367d791c031b97bb7ba317fc63c12f7
mahdi7ab <mahdi7ab@gmail.com>
2026-06-30 13:32:00 +0330

    fix: update

--- scanner/scanner.py
+++ scanner/scanner.py
@@ -11,7 +11,7 @@ from urllib.parse import urlparse, parse_qs
 from concurrent.futures import ThreadPoolExecutor
 
 SUBSCRIPTION_URLS = [
-    # "https://raw.githubusercontent.com/ebrasha/free-v2ray-public-list/refs/heads/main/V2Ray-Config-By-EbraSha.txt",
+    "https://raw.githubusercontent.com/ebrasha/free-v2ray-public-list/refs/heads/main/V2Ray-Config-By-EbraSha.txt",
     "https://github.com/ebrasha/free-v2ray-public-list/raw/refs/heads/main/separated-protocols-chunks/vless/EbraSha-Protocol-Chunks-vless-004.txt",
     "https://github.com/ebrasha/free-v2ray-public-list/raw/refs/heads/main/separated-protocols-chunks/vless/EbraSha-Protocol-Chunks-vless-008.txt",
     "https://github.com/ebrasha/free-v2ray-public-list/raw/refs/heads/main/separated-protocols-chunks/vless/EbraSha-Protocol-Chunks-vless-015.txt",
